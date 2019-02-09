33d425c7ce4b8e6fde40b6e8e0c08931684e713e
Puja Patel <pujap52@gmail.com>
2019-02-09 04:24:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarf_names)
-  dwarf_names.each_with_index { |name, order| puts " #{order + 1}. #{name}" }
+  dwarf_names[0...dwarf_names.length/2].each_with_index { |name, order| puts " #{order + 1}. #{name}" }
+  
 end
 
 def summon_captain_planet(planeteer_calls) 
@@ -12,7 +13,7 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(ingredients)
-  cheese_plate = ["cheddar", "gouda", "camembert"]
+  cheese_plate = ["cheddar", "gouda", "camembert", "swiss"]
   
   cheese_plate.each do |some_cheese|
     if ingredients.include?(some_cheese)
@@ -21,3 +22,26 @@ def find_the_cheese(ingredients)
   end
   return nil
 end
+
+
+def words_that_start_with_b(words)
+  b_words = [] # This will keep track of all the words that start with b
+  words.each do |word|
+    if word.start_with?("b")
+      b_words.push word
+    end
+  end
+  return b_words
+end
+
+# []
+
+# ['brian']
+# ['brian']
+# ['brian']
+
+words_that_start_with_b(['brian', 'joe', 'eric']) # ['brian']
+words_that_start_with_b(['banana', 'berry', 'cherry']) # ['banana', 'berry']
+words_that_start_with_b(['wonder woman', 'hulk', 'wasp']) # []
+
+
